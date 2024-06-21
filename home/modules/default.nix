a91a1e2536f1a1b7338b3bf280f46ae97ff41116
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-21 22:15:23 +0200

    added gtk in home modules

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./console
+    ./gtk
     ./screenshot
     ./sway
   ];
---
+++ home/modules/gtk/default.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  cnfg = config.home.modules.gtk;
+in
+{
+  options.home.modules.gtk.enable = mkEnableOption "gtk";
+
+  config = mkIf cnfg.enable {
+    home.packages = with pkgs; [
+      gnome.gnome-tweaks
+    ];
+
+    gtk = {
+      enable = true;
+
+      cursorTheme = {
+        name = "Vanilla-DMZ";
+        package = pkgs.vanilla-dmz;
+      };
+
+      font = {
+        name = "Inter";
+        size = 10;
+      };
+
+      iconTheme = {
+        name = "Fluent";
+        package = pkgs.fluent-icon-theme;
+      };
+
+      theme = {
+        name = "Fluent-Dark-compact";
+        package = pkgs.fluent-gtk-theme;
+      };
+
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+  };
+}
