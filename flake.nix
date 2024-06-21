72554e261c74671b4a989084d9d050aa7fda15da
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-21 15:14:05 +0200

    housekeeping

--- flake.nix
+++ flake.nix
@@ -1,28 +1,27 @@
 {
-  description = "A very basic flake";
+  description = "nix configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    home-manager = {
+    hardware.url = "github:NixOS/nixos-hardware/master";
+    home = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+  outputs = { self, hardware, nixpkgs, home, ... }:
     let
       user = "hujing";
-      system = "x86_64-linux";
-
+      
       pkgs = import nixpkgs {
-        inherit system;
         config.allowUnfree = true;
       };
     in
     {
       nixosConfigurations = {
         desktop = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit user inputs; };
+          system = "x86_64-linux";
           modules = [
             ./systems/desktop
             {
@@ -33,7 +32,7 @@
               };
             }
             
-            home-manager.nixosModules.home-manager {
+            home.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit user; };
