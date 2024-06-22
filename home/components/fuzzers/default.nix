5f2f20844f21fdcaed911efaeeb0536795bf4b20
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-22 17:07:36 +0200

    added fuzzers; stable

--- home/components/fuzzers/default.nix
+++ home/components/fuzzers/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 
 let
@@ -8,19 +8,21 @@ in
   options.home.components.fuzzers.enable = mkEnableOption "fuzzers";
 
   config = mkIf cnfg.enable {
-    packages = with pkgs; [
-      #afl
-      #aflplusplus
-      #feroxbuster
-      #ffuf
-      gobuster
-      #honggfuzz
-      #radamsa
-      #regexploit
-      #scout
-      #ssdeep
-      #wfuzz
-      #zzuf
-    ];
+    home = {
+      packages = with pkgs; [
+        #afl
+        #aflplusplus
+        #feroxbuster
+        #ffuf
+        gobuster
+        #honggfuzz
+        #radamsa
+        #regexploit
+        #scout
+        #ssdeep
+        #wfuzz
+        #zzuf
+      ];
+    };
   };
 }
