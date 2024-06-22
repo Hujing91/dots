36c2b3d3d6f2672e5701895e087f0d3a857057e0
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-22 20:38:28 +0200

    removed edge integration

--- home/modules/notification/default.nix
+++ home/modules/notification/default.nix
@@ -12,10 +12,10 @@ in
       components = {
         dunst.enable = true;
 
-        edge = {
-          enable = true;
-          enableDunstIntegration = true;
-        };
+        #edge = {
+        #  enable = true;
+        #  enableDunstIntegration = true;
+        #};
 
         rofi.enable = true;
       };
