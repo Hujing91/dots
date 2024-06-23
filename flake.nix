4d98c157460839607ec96c84104e0ccf9c7e34ea
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-23 23:31:36 +0200

    fixed reference from alienware starting point to alienware home file

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users."${user}" = import ./home/desktop.nix;
+              home-manager.users."${user}" = import ./home/alienware.nix;
               home-manager.backupFileExtension = "backup";
             }
           ];
