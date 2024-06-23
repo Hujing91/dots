c07532f4e902399425d1879a024f1b8d33ed22bd
Hujing91 <christianpetersohn91@googlemail.com>
2024-06-23 11:19:12 +0200

    changed wallpaper path in swww

--- home/components/swww/default.nix
+++ home/components/swww/default.nix
@@ -37,13 +37,13 @@ in
     home.modules.sway.prependedConfig = mkIf cnfg.enableSwayIntegration ''
       # Start swww daemon and cycle through random wallpaper
       exec swww-daemon
-      exec bash ~/.config/swww/wallpaper.sh ~/onedrive/Wallpapers/
+      exec bash ~/.config/swww/wallpaper.sh ~/hujing/Wallpapers/
     '';
 
     home.modules.hyprland.additionalConfig = mkIf cnfg.enableHyprlandIntegration ''
       # Start swww daemon and cycle through random wallpaper
       exec-once = swww-daemon
-      exec-once = bash ~/.config/swww/wallpaper.sh ~/onedrive/Wallpapers/
+      exec-once = bash ~/.config/swww/wallpaper.sh ~/hujing/Wallpapers/
     '';
   };
 }
